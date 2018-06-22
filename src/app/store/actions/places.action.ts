7a330bba8e796bbbc694588daf90c65f5bb0c06e
neighbor983 <kenneth.ray.baker@gmail.com>
2018-06-22 06:40:59 +0000

    misspelled implements

--- src/app/store/actions/places.action.ts
+++ src/app/store/actions/places.action.ts
@@ -6,17 +6,17 @@ export const ADD_PLACE = '[Place] Add Place';
 export const ADD_PLACE_FAIL = '[Place] Failed to Add Place';
 export const ADD_PLACE_SUCCESS = '[Place] Added Place Successfully';
 
-export class AddPlace implments Action {
+export class AddPlace implements  Action {
     readonly type = ADD_PLACE;
     constructor(public payload: Place) {}
 }
 
-export class AddPlaceFail implments Action {
+export class AddPlaceFail implements  Action {
     readonly type = ADD_PLACE_FAIL;
     constructor(public payload: any) {}
 }
 
-export class AddPlaceSuccess implments Action {
+export class AddPlaceSuccess implements  Action {
     readonly type = ADD_PLACE_SUCCESS;
     constructor(public payload: Place){}
 }
